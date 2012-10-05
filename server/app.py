9348cba4ec69bb73e6f29aced279f287e5ce25c9
lukin0110 <maarten.huijsmans@gmail.com>
2012-10-05 23:13:14 +0200

    init app

---
+++ server/app.py
@@ -0,0 +1,38 @@
+import logging
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+from language import pages
+
+
+# loading the mappings
+mappings = [
+    ('/', pages.IndexPage),
+    ]
+
+'''
+    ('/wie/([^\\.]*)', pages.PersonPage),
+    ('/spel/(.*)', pages.GamePage),
+    ('/.*', pages.CatchallPage),
+'''
+
+# initialize the app with the mappings
+application = webapp.WSGIApplication(mappings,debug=True)
+
+
+def main():
+    # Set the logging level in the main function
+    # See the section on Requests and App Caching for information on how
+    # App Engine reuses your request handlers when you specify a main function
+    logging.getLogger().setLevel(logging.DEBUG)
+    run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
---
+++ server/language/pages.py
@@ -0,0 +1,16 @@
+import base64
+import random
+import string
+import urllib
+from django.template import TemplateDoesNotExist
+
+import logging
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+
+class IndexPage(webapp.RequestHandler):
+    def get(self):
+        values = {}
+        self.response.out.write(template.render('templates/index.html', values))
+
