243062c88ca491fefb07183d790d0244a428a0b6
Jo Simoens <winfixit@gmail.com>
2012-10-06 14:18:10 +0200

    Removed inconsistencies from copy/pasting

--- server/language/pages.py
+++ server/language/pages.py
@@ -85,11 +85,6 @@ class SaveAssessment(webapp.RequestHandler):
         resultFromTTS = self.get('resultFromTTS')
         Assessment(phoneId = phoneId,sentenceId = sentenceId,resultFromTTS=resultFromTTS,score=score,date_created=datetime.now()).save()
 
-        phoneId = db.StringProperty(required=True)
-    sentenceId = db.StringProperty(required=True)
-    score = db.FloatProperty(required=True, default=-1)
-    date_created = db.DateTimeProperty()
-
     self.response.headers["Content-Type"] = "text/plain; charset=utf-8"
         self.response.out.write(json.dumps({"test": "d"}, indent=4))
 
