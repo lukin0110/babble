97aed05fe9ef7129e5a66d353f4c3ccdd02b967a
Kaarel Kaljurand <kaljurand@gmail.com>
2012-10-06 23:48:44 +0200

    Preliminary integration of the backend

---
+++ android/app/src/be/lukin/android/lang/backend/BabblePhrase.java
@@ -0,0 +1,6 @@
+package be.lukin.android.lang.backend;
+
+public interface BabblePhrase {
+	String getValue();
+	String getLocale();
+}
--- android/app/src/be/lukin/android/lang/backend/Sentence.java
+++ android/app/src/be/lukin/android/lang/backend/Sentence.java
@@ -1,57 +1,57 @@
 package be.lukin.android.lang.backend;
 
-public class Sentence {
-    private int id;
-    private String locale;
-    private String value;
-    private int group;
+public class Sentence implements BabblePhrase {
+	private int id;
+	private String locale;
+	private String value;
+	private int group;
 
-    public Sentence(int id, String locale, String value, int group) {
-        this.id = id;
-        this.locale = locale;
-        this.value = value;
-        this.group = group;
-    }
+	public Sentence(int id, String locale, String value, int group) {
+		this.id = id;
+		this.locale = locale;
+		this.value = value;
+		this.group = group;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getLocale() {
-        return locale;
-    }
+	public String getLocale() {
+		return locale;
+	}
 
-    public void setLocale(String locale) {
-        this.locale = locale;
-    }
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public void setValue(String value) {
-        this.value = value;
-    }
+	public void setValue(String value) {
+		this.value = value;
+	}
 
-    public int getGroup() {
-        return group;
-    }
+	public int getGroup() {
+		return group;
+	}
 
-    public void setGroup(int group) {
-        this.group = group;
-    }
+	public void setGroup(int group) {
+		this.group = group;
+	}
 
-    @Override
-    public String toString() {
-        return "Sentence{" +
-                "id=" + id +
-                ", locale='" + locale + '\'' +
-                ", value='" + value + '\'' +
-                ", group=" + group +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Sentence{" +
+				"id=" + id +
+				", locale='" + locale + '\'' +
+				", value='" + value + '\'' +
+				", group=" + group +
+				'}';
+	}
 }
